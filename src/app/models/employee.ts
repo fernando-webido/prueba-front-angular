de3e80d922cfccd8d1dc445055488b5246cba74e
Fernando Uribe <furibe@marivalaccess..com>
2019-11-06 11:32:05 -0600

    proyecto

---
+++ src/app/models/employee.ts
@@ -0,0 +1,11 @@
+export class Employee {
+    id: number = 0;
+    name: string;
+    country: string;
+    title: string;
+    respo: string [];
+}
+export class Responsa {
+    id: number = 0;
+    resp: string;
+}
